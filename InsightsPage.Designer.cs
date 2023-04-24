20cfa7114b5db02ee48027f19ab5b421ffc97b7f
neal mistry <neal.mistry3@gmail.com>
2023-04-24 19:04:43 +0100

    completed the insights

--- InsightsPage.Designer.cs
+++ InsightsPage.Designer.cs
@@ -28,48 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Querybutton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.kryptonButton2 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // Querybutton
-            // 
-            this.Querybutton.Location = new System.Drawing.Point(81, 177);
-            this.Querybutton.Name = "Querybutton";
-            this.Querybutton.Size = new System.Drawing.Size(166, 47);
-            this.Querybutton.TabIndex = 0;
-            this.Querybutton.Text = "Talk to me";
-            this.Querybutton.UseVisualStyleBackColor = true;
-            this.Querybutton.Click += new System.EventHandler(this.Querybutton_Click);
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.Controls.Add(this.richTextBox2);
-            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.Location = new System.Drawing.Point(12, 230);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.kryptonButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.kryptonButton2);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(312, 260);
-            this.panel1.TabIndex = 1;
+            this.panel1.Size = new System.Drawing.Size(413, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Powered by OpenAi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::dddProject2.Properties.Resources.download__1_1;
+            this.pictureBox1.Location = new System.Drawing.Point(165, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // kryptonButton2
+            // 
+            this.kryptonButton2.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.Gallery;
+            this.kryptonButton2.Location = new System.Drawing.Point(255, 22);
+            this.kryptonButton2.Name = "kryptonButton2";
+            this.kryptonButton2.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2007Silver;
+            this.kryptonButton2.Size = new System.Drawing.Size(143, 57);
+            this.kryptonButton2.TabIndex = 4;
+            this.kryptonButton2.Values.Text = "Return home";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(397, 22);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(13, 57);
+            this.panel4.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(3, 22);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(13, 57);
+            this.panel3.TabIndex = 3;
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.Gallery;
+            this.kryptonButton1.Location = new System.Drawing.Point(13, 22);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2007Silver;
+            this.kryptonButton1.Size = new System.Drawing.Size(143, 57);
+            this.kryptonButton1.TabIndex = 8;
+            this.kryptonButton1.Values.Text = "Ask away";
+            this.kryptonButton1.Click += new System.EventHandler(this.kryptonButton1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(29, 55);
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 100);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(276, 106);
-            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Size = new System.Drawing.Size(199, 530);
+            this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(17, 23);
+            this.richTextBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox2.Location = new System.Drawing.Point(208, 100);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(276, 223);
-            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Size = new System.Drawing.Size(205, 530);
+            this.richTextBox2.TabIndex = 4;
             this.richTextBox2.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(0, 636);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(199, 28);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Location = new System.Drawing.Point(208, 636);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(205, 28);
+            this.panel5.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Type in the textbox above how you feel.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "I will respond with advice.";
             // 
             // InsightsPage
             // 
@@ -77,25 +184,42 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(336, 502);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(413, 661);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.Querybutton);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "InsightsPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "InsightsPage";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Querybutton;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label3;
     }
 }
--- InsightsPage.cs
+++ InsightsPage.cs
@@ -6,34 +6,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace dddProject2
 {
     public partial class InsightsPage : Form
     {
 
-         
+
         public InsightsPage()
         {
             InitializeComponent();
         }
 
-        private async void Querybutton_Click(object sender, EventArgs e)
+      
+
+        private void label2_Click(object sender, EventArgs e)
         {
-            var openAI = new OpenAIAPI("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");
+
+        }
+
+        private async void kryptonButton1_Click(object sender, EventArgs e)
+        {
+            var openAI = new OpenAIAPI("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");
             CompletionRequest completionRequest = new CompletionRequest();
-            completionRequest.Prompt = richTextBox1.Text; //the request is taken from this textbox that the user writes in.
+            completionRequest.Prompt = richTextBox1.Text;
             completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
             completionRequest.MaxTokens = 2000;
-
             var completions = await openAI.Completions.CreateCompletionAsync(completionRequest);
 
             foreach (var completion in completions.Completions)
             {
                 richTextBox2.Text = completion.Text;
             }
-
         }
     }
 }
-
