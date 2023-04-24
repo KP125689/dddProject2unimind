45fd40deed2ce26e2ee53df2fcf9a39c2d28f85b
NealMistry <neal.mistry3@gmail.com>
2023-04-24 01:48:40 +0100

    completed the talk to us section

--- InsightsPage.Designer.cs
+++ InsightsPage.Designer.cs
@@ -28,23 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Querybutton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Querybutton
+            // 
+            this.Querybutton.Location = new System.Drawing.Point(81, 177);
+            this.Querybutton.Name = "Querybutton";
+            this.Querybutton.Size = new System.Drawing.Size(166, 47);
+            this.Querybutton.TabIndex = 0;
+            this.Querybutton.Text = "Talk to me";
+            this.Querybutton.UseVisualStyleBackColor = true;
+            this.Querybutton.Click += new System.EventHandler(this.Querybutton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.richTextBox2);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(12, 230);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(312, 260);
+            this.panel1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 55);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(276, 106);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(17, 23);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(276, 223);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
             // 
             // InsightsPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(448, 618);
+            this.ClientSize = new System.Drawing.Size(336, 502);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Querybutton);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "InsightsPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "InsightsPage";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Querybutton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
--- InsightsPage.cs
+++ InsightsPage.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using OpenAI_API.Completions;
+using OpenAI_API;
+using System.Windows.Forms;
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+
 
 namespace dddProject2
 {
     public partial class InsightsPage : Form
     {
+
+
         public InsightsPage()
         {
             InitializeComponent();
         }
+
+        private async void Querybutton_Click(object sender, EventArgs e)
+        {
+            var openAI = new OpenAIAPI("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");
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = richTextBox1.Text;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+            completionRequest.MaxTokens = 2000;
+
+            var completions = await openAI.Completions.CreateCompletionAsync(completionRequest);
+
+            foreach (var completion in completions.Completions)
+            {
+                richTextBox2.Text = completion.Text;
+            }
+
+        }
     }
 }
+
